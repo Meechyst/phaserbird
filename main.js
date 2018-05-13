2e54974eb184f677a3be85c63803837f0e58cc0b
fatih <meechysm@windowslive.com>
2018-05-14 00:26:23 +0300

    Make bird rotate as it moves up and down and add die functionality instead of restarting the game

--- main.js
+++ main.js
@@ -44,6 +44,9 @@ let mainState = {
     this.labelScore = game.add.text(20, 20, "0",
       { font: "30px Arial", fill: "#ffffff" });
 
+    // Move the anchor to the left and downward
+    this.bird.anchor.setTo(-0.2, 0.5);
+
   },
   // Contains game logic. Refreshes 60 times per second.
   update() {
@@ -52,9 +55,12 @@ let mainState = {
     if (this.bird.y < 0 || this.bird.y > 490)
       this.restartGame();
 
-    // Restarts the game if the bird overlaps the pipe.
+    // Calls hitPipe if the bird overlaps the pipe.
     game.physics.arcade.overlap(
-      this.bird, this.pipes, this.restartGame, null, this);
+      this.bird, this.pipes, this.hitPipe, null, this);
+
+    if (this.bird.angle < 20)
+      this.bird.angle += 1;
   },
 
   /*
@@ -94,14 +100,44 @@ let mainState = {
   },
   // Makes the bird jump
   jump() {
+
+    if (this.bird.alive === false)
+      return;
+
     // Adds a vertical velocity to the bird
     this.bird.body.velocity.y = -350;
+
+    // Creates an animation on the bird
+    let animation = game.add.tween(this.bird);
+
+    // Changes the angle of the bird to -20° in 100 milliseconds
+    animation.to({angle: -20}, 100);
+
+    // And start the animation
+    animation.start();
   },
 
   // Restarts the game
   restartGame() {
     // Starts the main state, which restarts the game
     game.state.start('main');
+  },
+  hitPipe: function() {
+    // >If the bird has already hit a pipe, do nothing
+    // >It means the bird is already falling off the screen
+    if (this.bird.alive === false)
+      return;
+
+    // Sets the alive property of the bird to false
+    this.bird.alive = false;
+
+    // Prevents new pipes from appearing
+    game.time.events.remove(this.timer);
+
+    // Goes through all the pipes, and stops their movement
+    this.pipes.forEach(function(p){
+      p.body.velocity.x = 0;
+    }, this);
   },
 
 };
